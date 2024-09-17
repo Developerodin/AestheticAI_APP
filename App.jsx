e57b0956fc1be718c0d6a629223b6dfc3f8f7178
Abhishekanjana <abhishekanjana85@gmail.com>
2024-09-17 17:32:56 +0530

    working

--- App.jsx
+++ App.jsx
@@ -236,16 +236,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  shadow: {
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.5,
-    elevation: 5,
-  },
+
   gradientIcon: {
     width: 40,
     height: 40,
